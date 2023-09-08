2628c8df31f6f8b3f5a0631eecc45fc8a120090b
Ryan Kendall <ryankendallt@gmail.com>
2023-09-08 11:28:52 +0100

    feat(1660): remove energy interrupt

--- app/interruptionPages.js
+++ app/interruptionPages.js
@@ -1,10 +1,15 @@
+// NOTE: interruption does not "intercept" the request, it replaces the options value in the form for normal submission.
+// See: app/dbTree/dbTreeQuestion.js:33
+
+// Example list value:
+// {
+//   interruptPath: '/find/type/on-going/services-categories/energy', - The original path - if an option value has this path, then it will be replaced
+//   redirectPath: '/interruptions/rising-energy-prices',             - Go here instead (routed in "routeInterruptionPages")
+//   title: 'What you can do to reduce energy costs'                  - The title of the resulting interruption page
+//   template: 'interruptions/rising-energy-prices.njk',              - The template of the resulting interruption page
+// }
+
 const list = [
-  {
-    interruptPath: '/find/type/on-going/services-categories/energy',
-    template: 'interruptions/rising-energy-prices.njk',
-    redirectPath: '/interruptions/rising-energy-prices',
-    title: 'What you can do to reduce energy costs'
-  }
 ]
 
 const getInterruptionPath = (path) => {
